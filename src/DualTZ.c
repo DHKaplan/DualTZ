e6b9e133997717ea312ad1b9da59a723122daf24
DHKaplan <wa1oui.ham@gmail.com>
2015-11-29 10:23:19 -0600

    Added Temperature

--- src/DualTZ.c
+++ src/DualTZ.c
@@ -1,7 +1,10 @@
 #include "pebble.h"
+#include <inttypes.h>
 
-#define DATE_STYLE 0
-#define BT_VIBRATE 1
+#define DATE_FORMAT_KEY 0
+#define BT_VIBRATE_KEY  1
+#define WEATHER_TEMPERATURE_KEY 2
+#define WEATHER_CITY_KEY 3
 
 Window *window;
 
@@ -11,10 +14,10 @@ TextLayer *text_time_layer;
 TextLayer *text_time2_layer;
 TextLayer *text_location_layer;
 TextLayer *text_location2_layer;
+TextLayer *text_degrees_layer;
 
 Layer        *BatteryLineLayer;
 
-GFont        fontHelvNewLight15;
 GFont        fontMonaco13;
 GFont		     fontRobotoCondensed21;
 GFont        fontRobotoBoldSubset40;
@@ -38,12 +41,13 @@ static char date_text[] = "Mon 05/08/48";
 static char date2_text[] = "Mon 05/08/48";
 static char seconds_text[] = "00";
 static char date_format[]="%a %m/%d/%y";
-static char date2_format[]="%a %m/%d/%y";
 static char text_location[18];
 static char text_location2[18];
+static char text_degrees[] = "====";
 
-time_t rawtime;
-struct tm *gmtinfo;
+
+//time_t rawtime;
+//struct tm *gmtinfo;
 
 GColor TextColorHold;
 GColor BGColorHold;
@@ -76,37 +80,51 @@ void handle_battery(BatteryChargeState charge_state) {
     layer_mark_dirty(BatteryLineLayer);
 }
 
-void battery_line_layer_update_callback(Layer *BatteryLineLayer, GContext* ctx) { 
-     graphics_context_set_fill_color(ctx, TextColorHold);
-     graphics_fill_rect(ctx, layer_get_bounds(BatteryLineLayer), 3, GCornersAll);
+void battery_line_layer_update_callback(Layer *BatteryLineLayer, GContext* batctx) { 
+     graphics_context_set_fill_color(batctx, TextColorHold);
+     graphics_fill_rect(batctx, layer_get_bounds(BatteryLineLayer), 3, GCornersAll);
 
      if (batterycharging == 1) {
        #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorBlue);
+          graphics_context_set_fill_color(batctx, GColorBlue);
        #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
+          graphics_context_set_fill_color(batctx, GColorBlack);
        #endif
 
-       graphics_fill_rect(ctx, GRect(2, 1, 100, 4), 3, GCornersAll);
+       graphics_fill_rect(batctx, GRect(2, 1, 100, 4), 3, GCornersAll);
 
      } else if (batterychargepct > 20) {
        #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorGreen);
+          graphics_context_set_fill_color(batctx, GColorGreen);
        #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
+          graphics_context_set_fill_color(batctx, GColorBlack);
        #endif
          
-       graphics_fill_rect(ctx, GRect(2, 1, batterychargepct, 4), 3, GCornersAll);
+       graphics_fill_rect(batctx, GRect(2, 1, batterychargepct, 4), 3, GCornersAll);
        
      } else {
       #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorRed);
+          graphics_context_set_fill_color(batctx, GColorRed);
        #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
+          graphics_context_set_fill_color(batctx, GColorBlack);
        #endif
          
-       graphics_fill_rect(ctx, GRect(2, 1, batterychargepct, 4),3, GCornersAll);
+       graphics_fill_rect(batctx, GRect(2, 1, batterychargepct, 4),3, GCornersAll);
      }
+  
+  //Battery % Markers
+      graphics_context_set_fill_color(batctx, GColorWhite);
+      graphics_fill_rect(batctx, GRect(89, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(79, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(69, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(59, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(49, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(39, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(29, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(19, 1, 3, 4), 3, GCornerNone);
+      graphics_fill_rect(batctx, GRect(9, 1, 3, 4), 3, GCornerNone);
+
+
 }
 void handle_bluetooth(bool connected) {
       if (connected) {
@@ -130,7 +148,7 @@ void handle_appfocus(bool in_focus){
         handle_bluetooth(bluetooth_connection_service_peek());
     }
 }
-//<============================================================================================
+
 void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
 
   char time_format[] = "%I:%M";
@@ -184,30 +202,29 @@ void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
      text_layer_set_text(text_time_layer,  time_text);
      text_layer_set_text(text_time2_layer, time2_text);  
      }
-  strcpy(text_location, "San Antonio, TX");
   strcpy(text_location2, "West Simsbury, CT");
 
   FirstTime = 1;
   
 }  
-
-//Receive Input from Config html page:
+//<============================================================================================
+//Receive Input from Config html page and location/temp:
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   char BTVibeConfig[] = "0";
 
   APP_LOG(APP_LOG_LEVEL_INFO, "In Inbox received callback");
 
-  FirstTime = 0;
 
   // Read first item
   Tuple *t = dict_read_first(iterator);
 
   // For all items
   while(t != NULL) {
-
     // Which key was received?
     switch(t->key) {
-    case 0:
+      APP_LOG(APP_LOG_LEVEL_INFO, "string = %s", t->value->cstring);
+ 
+      case 0: // Date Format
       strcpy(date_type, t->value->cstring);
 
       if (strcmp(date_type, "us") == 0) {
@@ -216,25 +233,62 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
          strcpy(date_format, "%e %b %Y");
       }
       text_layer_set_text(text_date_layer, date_text);
+      APP_LOG(APP_LOG_LEVEL_INFO, "Processed DATE_FORMAT_KEY");
+
       break;
 
-    case 1:
+    case 1: // BT Vibe
       strcpy(BTVibeConfig, t->value->cstring);
       if (strcmp(BTVibeConfig, "0") == 0) {
          strcpy(VibOnBTLoss,"0");
       } else {
          strcpy(VibOnBTLoss,"1");
       }
+      APP_LOG(APP_LOG_LEVEL_INFO, "Processed BT_VIBRATE_KEY");
+
       break;
 
-    default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-      break;
+    case 2: //Temp
+      strcpy(text_degrees,(t->value->cstring));
+  
+
+      if (strcmp((text_degrees), "N/A") != 0) {
+        int tempint = 100;
+  
+        tempint = atoi(text_degrees);
+  
+        tempint = ((tempint * 9) / 5) + 32;
+  
+         // Assemble full string and display
+         snprintf(text_degrees, 5, "%dF", tempint);
+         }  
+        else {
+          strcpy(text_degrees, "N/A");  
+        } 
+        text_layer_set_text(text_degrees_layer, text_degrees); 
+      
+        APP_LOG(APP_LOG_LEVEL_INFO, "c Processed Degrees -> %s", text_degrees);
+        break;
+      
+        case 3: //Location
+       //Read City
+       strcpy(text_location,(t->value->cstring));
+
+       text_layer_set_text(text_location_layer, text_location);
+  
+       APP_LOG(APP_LOG_LEVEL_INFO, "c Processed Location = %s", text_location);
+       break;
+      
+      default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+        break;  
     }
-
+  
     // Look for next item
     t = dict_read_next(iterator);
-  }
+    
+       FirstTime = 0;
+    }
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
@@ -252,12 +306,14 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 void handle_deinit(void) {
   tick_timer_service_unsubscribe();
 
-  persist_write_string(DATE_STYLE, date_type);
-  persist_write_string(BT_VIBRATE, VibOnBTLoss);
+  persist_write_string(DATE_FORMAT_KEY, date_type);
+  persist_write_string(BT_VIBRATE_KEY, VibOnBTLoss);
 
   battery_state_service_unsubscribe();
   bluetooth_connection_service_unsubscribe();
   app_focus_service_unsubscribe();
+  app_message_deregister_callbacks();
+
 
   text_layer_destroy(text_time_layer);
   text_layer_destroy(text_time2_layer);
@@ -265,8 +321,10 @@ void handle_deinit(void) {
   text_layer_destroy(text_date2_layer);
   text_layer_destroy(text_location_layer);
   text_layer_destroy(text_location2_layer);
+  text_layer_destroy(text_degrees_layer);
+  layer_destroy(BatteryLineLayer);
 
-  fonts_unload_custom_font(fontHelvNewLight15);
+
   fonts_unload_custom_font(fontMonaco13);
   fonts_unload_custom_font(fontRobotoBoldSubset40);
   fonts_unload_custom_font(fontRobotoBoldSubset45);
@@ -276,18 +334,20 @@ void handle_deinit(void) {
 
 void handle_init(void) {
 
-  GColor BGCOLOR   = COLOR_FALLBACK(GColorDukeBlue, GColorBlack);
-  BGColorHold = BGCOLOR;
+  GColor BGCOLOR1   = COLOR_FALLBACK(GColorKellyGreen, GColorBlack);
+  BGColorHold = BGCOLOR1;
+  
+  GColor BGCOLOR2   = COLOR_FALLBACK(GColorDukeBlue, GColorBlack);
+  BGColorHold = BGCOLOR1;
 
   GColor TEXTCOLOR = COLOR_FALLBACK(GColorWhite, GColorWhite);
   TextColorHold = TEXTCOLOR;
 
   window = window_create();
   window_stack_push(window, true /* Animated */);
-  window_set_background_color(window, BGCOLOR);
+  window_set_background_color(window, BGCOLOR1);
 
   fontMonaco13           = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MONACO_13));
-  fontHelvNewLight15     = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELV_NEW_LIGHT_15));
   fontRobotoBoldSubset40 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_40));
   fontRobotoBoldSubset45 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_45));
   Layer *window_layer = window_get_root_layer(window);
@@ -301,33 +361,36 @@ void handle_init(void) {
   // Open AppMessage
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
  
-  //Location
-  text_location_layer = text_layer_create(GRect(1, 1, 144, 17)); 
+  //Location 1
+  text_location_layer = text_layer_create(GRect(1, 1, 110, 17)); 
   text_layer_set_text_alignment(text_location_layer, GTextAlignmentCenter);		
   text_layer_set_text(text_location_layer, text_location); 
   text_layer_set_font(text_location_layer, fontMonaco13);
-  text_layer_set_background_color(text_location_layer, GColorKellyGreen);
+  text_layer_set_background_color(text_location_layer, BGCOLOR1);
   text_layer_set_text_color(text_location_layer, TEXTCOLOR);
   layer_add_child(window_layer, text_layer_get_layer(text_location_layer));
   
+  //Temperature
+  text_degrees_layer = text_layer_create(GRect(111, 1, 35, 17)); 
+  text_layer_set_text_alignment(text_degrees_layer, GTextAlignmentRight);		
+  text_layer_set_text(text_degrees_layer, text_degrees); 
+  text_layer_set_font(text_degrees_layer, fontMonaco13);
+  text_layer_set_background_color(text_degrees_layer, BGCOLOR1);
+  text_layer_set_text_color(text_degrees_layer, TEXTCOLOR);
+  layer_add_child(window_layer, text_layer_get_layer(text_degrees_layer));
   
-// Date
-  #ifdef PBL_PLATFORM_CHALK
-     text_date_layer = text_layer_create(GRect(1, 18, 180, 28));
-  #else
-     text_date_layer = text_layer_create(GRect(1, 18, 144, 22));
-  #endif
-
+  // Date 1
+  text_date_layer = text_layer_create(GRect(1, 18, 144, 22));
   text_layer_set_text_color(text_date_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_date_layer, GColorKellyGreen);
+  text_layer_set_background_color(text_date_layer, BGCOLOR1);
   text_layer_set_text_alignment(text_date_layer, GTextAlignmentCenter);;
   text_layer_set_font(text_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
 
   
   //Persistent Value Date Format:
-  if (persist_exists(DATE_STYLE)) {
-     persist_read_string(DATE_STYLE, date_type, sizeof(date_type));
+  if (persist_exists(DATE_FORMAT_KEY)) {
+     persist_read_string(DATE_FORMAT_KEY, date_type, sizeof(date_type));
   }  else {
      strcpy(date_type, "us");
   }
@@ -339,32 +402,26 @@ void handle_init(void) {
   }
 
   //Persistent Value VibOnBTLoss
-  if(persist_exists(BT_VIBRATE)) {
-     persist_read_string(BT_VIBRATE, VibOnBTLoss, sizeof(VibOnBTLoss));
+  if(persist_exists(BT_VIBRATE_KEY)) {
+     persist_read_string(BT_VIBRATE_KEY, VibOnBTLoss, sizeof(VibOnBTLoss));
   }  else {
      strcpy(VibOnBTLoss, "0"); // Default
   }
 
-  // Time of Day
-  #ifdef PBL_PLATFORM_CHALK
-      text_time_layer = text_layer_create(GRect(1, 120, 180, 45));
-      text_layer_set_font(text_time_layer,fontRobotoBoldSubset40);
-  #else
-      text_time_layer = text_layer_create(GRect(1, 40, 144, 40));
-      text_layer_set_font(text_time_layer,fontRobotoBoldSubset40);
-  #endif
-
+  // Time of Day 1
+  text_time_layer = text_layer_create(GRect(1, 40, 144, 40));
+  text_layer_set_font(text_time_layer,fontRobotoBoldSubset40);
   text_layer_set_text_color(text_time_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_time_layer, GColorKellyGreen);
+  text_layer_set_background_color(text_time_layer, BGCOLOR1);
   text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
-   //Location2
+   //Location 2
   text_location2_layer = text_layer_create(GRect(1, 92, 144, 17)); 
   text_layer_set_text_alignment(text_location2_layer, GTextAlignmentCenter);		
   text_layer_set_text(text_location2_layer, text_location2); 
   text_layer_set_font(text_location2_layer, fontMonaco13);
-  text_layer_set_background_color(text_location2_layer, BGCOLOR);
+  text_layer_set_background_color(text_location2_layer, BGCOLOR2);
   text_layer_set_text_color(text_location2_layer, TEXTCOLOR);
   layer_add_child(window_layer, text_layer_get_layer(text_location2_layer));
   
@@ -374,30 +431,20 @@ void handle_init(void) {
   layer_set_update_proc(BatteryLineLayer, battery_line_layer_update_callback);
   layer_add_child(window_layer, BatteryLineLayer);
 
-  // Date2
-  #ifdef PBL_PLATFORM_CHALK
-     text_date2_layer = text_layer_create(GRect(1, 18, 180, 28));
-  #else
-     text_date2_layer = text_layer_create(GRect(1, 106, 144, 22));
-  #endif
-
+  // Date 2
+  text_date2_layer = text_layer_create(GRect(1, 106, 144, 22));
   text_layer_set_text_color(text_date2_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_date2_layer, BGCOLOR);
+  text_layer_set_background_color(text_date2_layer, BGCOLOR2);
   text_layer_set_text_alignment(text_date2_layer, GTextAlignmentCenter);;
   text_layer_set_font(text_date2_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   layer_add_child(window_layer, text_layer_get_layer(text_date2_layer));
 
-/*   // Time of Day2
-  #ifdef PBL_PLATFORM_CHALK
-      text_time_layer = text_layer_create(GRect(1, 120, 180, 45));
-      text_layer_set_font(text_time_layer,fontRobotoBoldSubset40);
-  #else */
-      text_time2_layer = text_layer_create(GRect(1, 127, 144, 40));
-      text_layer_set_font(text_time2_layer,fontRobotoBoldSubset40);
-//  #endif
+  // Time 2
+  text_time2_layer = text_layer_create(GRect(1, 127, 144, 40));
+  text_layer_set_font(text_time2_layer,fontRobotoBoldSubset40);
 
   text_layer_set_text_color(text_time2_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_time2_layer, BGCOLOR);
+  text_layer_set_background_color(text_time2_layer, BGCOLOR2);
   text_layer_set_text_alignment(text_time2_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_time2_layer)); 
 
